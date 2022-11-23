e4154aea7884d0cc1ad440daf96c4f25c085a165
Sasha The Gray <contact-sasha@proton.me>
2022-11-23 20:50:15 +0000

    chore: Hook listSlice up to the Redux store

--- src/redux/features/index.ts
+++ src/redux/features/index.ts
@@ -1,4 +1,7 @@
 import * as boardActions from "./boardSlice"
+import * as listActions from "./listSlice"
 import boardReducer from "./boardSlice"
+import listReducer from "./listSlice"
 
 export {boardActions, boardReducer}
+export {listActions, listReducer}
