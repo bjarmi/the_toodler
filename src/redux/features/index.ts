e42a83204297ba0188bc8367f026ace8b1ba39a5
Sasha The Gray <contact-sasha@proton.me>
2022-11-23 21:06:37 +0000

    feat: Add taskSlice

--- src/redux/features/index.ts
+++ src/redux/features/index.ts
@@ -1,7 +1,10 @@
 import * as boardActions from "./boardSlice"
 import * as listActions from "./listSlice"
+import * as taskActions from "./taskSlice"
 import boardReducer from "./boardSlice"
 import listReducer from "./listSlice"
+import taskReducer from "./taskSlice"
 
 export {boardActions, boardReducer}
 export {listActions, listReducer}
+export {taskActions, taskReducer}
