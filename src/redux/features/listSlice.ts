9e2b5f412d5eaef8b3b13123f68f9227e2e5c6d4
Sasha The Gray <contact-sasha@proton.me>
2022-11-23 20:49:59 +0000

    feat: Add listSlice

---
+++ src/redux/features/listSlice.ts
@@ -0,0 +1,78 @@
+/*
+* This module contains the listSlice.
+*/
+
+import {createSlice, Slice} from "@reduxjs/toolkit";
+import {IDepartmentAction} from "../interfaces";
+import {IList} from "../../common/interfaces";
+import {ListDoesNotExistError, IncorrectActionTypeError} from "../exceptions";
+import {StyleSheet} from "react-native";
+
+// Define an interface for the listSlice.
+interface IListDepartment {
+  lists: Set<IList>,
+  next_id: number
+}
+
+// Define the initial state for the List department.
+const initialState: IListDepartment = {
+  lists: new Set(),
+  next_id: 1
+}
+
+const listSlice: Slice = createSlice({
+  name: "lists",
+  initialState: initialState,
+  reducers: {
+
+    addList: (department: IListDepartment, action: IDepartmentAction): void => {
+      // Validate action type.
+      if (action.type === "addList")
+        department.lists.add(action.payload)
+      else
+        throw new IncorrectActionTypeError("addBoard", action.type)
+    },
+
+    editList: (department: IListDepartment, action: IDepartmentAction): void => {
+      // Validate action type.
+      if (action.type !== "editList")
+        throw new IncorrectActionTypeError("editList", action.type)
+
+      // Change list if it exists.
+      let listFound: boolean = false
+      for (let list of department.lists)
+        if (list.id == action.payload.id) {
+          listFound = true
+          list = action.payload
+        }
+
+      // Throw error if the list was not found.
+      if (!listFound)
+        throw new ListDoesNotExistError(action.payload.id)
+    },
+
+    removeList: (department: IListDepartment, action: IDepartmentAction): void => {
+      // Validate action type.
+      if (action.type !== "removeList")
+        throw new IncorrectActionTypeError("removeList", action.type)
+
+      // Check if list exists.
+      let listFound: boolean = false
+      for (const list of department.lists)
+        if (list.id === action.payload.id) {
+          listFound = true
+          break
+        }
+
+      // Delete list.
+      if (listFound)
+        department.lists.delete(action.payload)
+      else
+        throw new ListDoesNotExistError(action.payload.id)
+    },
+  }
+})
+
+// Export reducer actions.
+export const {addList, editList, removeList} = listSlice.actions
+export default listSlice.reducer
