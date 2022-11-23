2268247edfe34fcb945c408d2009a98d949f3a41
Sasha The Gray <contact-sasha@proton.me>
2022-11-23 21:58:30 +0000

    docs: Add documentation for Redux interfaces

--- src/redux/interfaces.ts
+++ src/redux/interfaces.ts
@@ -2,7 +2,14 @@
 * This module contains interfaces for Redux.
 */
 
-// Define an interface for department actions.
+/**
+ * This interface represents actions within the Redux store.
+ *
+ * @interface IDepartmentAction
+ * @member {any} payload The payload of the action (ex. an instance of IBoard or IList).
+ * @member {string} type The type of this action (ex. "addTask" or "removeList").
+ * @author Alexander Robertson -> contact-sasha@proton.me
+ */
 export interface IDepartmentAction {
   payload: any,
   type: string
