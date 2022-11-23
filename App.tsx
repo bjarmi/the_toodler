6ebd9c79fabdbada9251ff690a04551cd2602489
Bjarmi <bjarmiae@gmail.com>
2022-11-23 21:34:21 +0000

    Add navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { BoardRoute, HomeRoute, ListRoute } from "./src/common/constants";
+import BoardPage from "./src/components/pages/boardPage";
+import HomePage from "./src/components/pages/homePage";
+import ListPage from "./src/components/pages/listPage";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={HomeRoute}>
+        <Stack.Screen
+          name={HomeRoute}
+          component={HomePage}
+          options={{ title: "The Toodler" }}
+        />
+        <Stack.Screen name={BoardRoute} component={BoardPage} />
+        <Stack.Screen name={ListRoute} component={ListPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
