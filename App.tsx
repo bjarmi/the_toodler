901daa3d761cb61a2fe0befab91b3d8d6018c902
Bjarmi <bjarmiae@gmail.com>
2022-11-26 15:06:20 +0000

    Add Redux provider

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import BoardPage from "./src/components/pages/boardPage";
 import HomePage from "./src/components/pages/homePage";
 import ListPage from "./src/components/pages/listPage";
 import { PageParams } from "./src/common/type";
+import { store } from "./src/redux/store";
+import { Provider } from "react-redux";
 
 // Allow Redux store to use immutable objects.
 // https://immerjs.github.io/immer/complex-objects/
@@ -13,16 +15,18 @@ const Stack = createNativeStackNavigator<PageParams>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomePage">
-        <Stack.Screen
-          name="HomePage"
-          component={HomePage}
-          options={{ title: "The Toodler" }}
-        />
-        <Stack.Screen name="BoardPage" component={BoardPage} />
-        <Stack.Screen name="ListPage" component={ListPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="HomePage">
+          <Stack.Screen
+            name="HomePage"
+            component={HomePage}
+            options={{ title: "The Toodler" }}
+          />
+          <Stack.Screen name="BoardPage" component={BoardPage} />
+          <Stack.Screen name="ListPage" component={ListPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
